84190fc69015d2b250302deac97b7355ed617b33
Renz <renzgabuay@gmail.com>
2025-08-26 21:40:43 +0800

    Add drag event listeners to lists
    Attached dragover, dragenter, dragleave, and drop event
    listeners to each list to enable card placement during
    drag-and-drop.

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ for (const card of cards) {
   card.addEventListener("dragstart", dragStart);
   card.addEventListener("dragend", dragEnd);
 }
+
+for (const list of lists) {
+  list.addEventListener("dragover", dragOver);
+  list.addEventListener("dragenter", dragEnter);
+  list.addEventListener("dragleave", dragLeave);
+  list.addEventListener("drop", drop);
+}
